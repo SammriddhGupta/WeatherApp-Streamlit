9abe4ee7a445f33b685f1a3ad2114b373e43aff5
SammriddhGupta <sammriddhgupta@gmail.com>
2023-01-14 23:23:44 +0800

    initial setup

---
+++ weather-app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import requests
+from datetime import datetime,time
+import pandas as pd
+
+api_key = st.secrets["api_key"]
+
+url_current = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+
+url_oneCall = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+
+def getweather(city):
+    result = requests.get(url_current.format(city, api_key))
+    if result:
+        json = result.json()
+        country = json['sys']['country']
+        temp = json['main']['temp'] - 273.15
+        temp_feels = json['main']['feels_like'] - 273.15
+        humid = json['main']['humidity'] - 273.15
+        icon = json['weather'][0]['icon']
+        lon = json['coord']['lon']
+        lat = json['coord']['lat']
+        des = json['weather'][0]['description']
+        res = [country, round(temp,1), round(temp_feels,1),humid,lon,lat,icon,des]
+        return res, json
+    else:
+        print("Error in Search!")
+        
+
+def get_hist_data(lat, lon, start):
+    res = requests.get(url_oneCall.format(lat, lon, start, api_key))
+    data = res.json()
+    temp = []
+    for hour in data["hourly"]:
+        t = hour["temp"]
+        temp.append(t)
+        return data,temp
+    
+# writing the streamlit app
+
+st.title("Sam's Weather Reporting")
+
+im1 = st.beta_columns(1)
+
+with im1:
+    image = 'img.png'
+    st.image(image, use_column_width=True, caption="This is our stack")
